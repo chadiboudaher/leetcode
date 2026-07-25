a4c2491e088671f41523eb7d16a42f5358c41126
Chadi Boudaher <chadiboudaher7@gmail.com>
2026-07-25 20:53:40 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (84.15%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countSegments(self, s: str) -> int:
+        s = s.strip()
+        if len(s) == 0:
+            return 0
+
+        count = 1
+        for i in range(len(s)):
+            if s[i] == " " and s[i - 1] != " ":
+                count += 1
+
+        return count
