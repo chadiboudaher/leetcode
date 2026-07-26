976f621fae68f62ba01a3f5f22506659084e0fff
Chadi Boudaher <chadiboudaher7@gmail.com>
2026-07-26 14:51:56 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.06%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,16 +1,13 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         nums.sort()
-        match = True
         round = 0
         output = 0
 
         for i in range(len(nums) - 1):
             if nums[i] == nums[i+1]:
                 output = output + round + 1
-                match = True
                 round += 1
             else:
-                match = False
                 round = 0
         return output
