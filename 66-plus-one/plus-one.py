9b86b4ec505480781323d796adbd90e15cedf50f
Chadi Boudaher <chadiboudaher7@gmail.com>
2026-08-18 20:38:09 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.68%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,5 +1,11 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        res = int("".join(map(str, digits)))
-        res += 1
-        return [int(i) for i in str(res)]
+    def plusOne(self, digits: list[int]) -> list[int]:
+        # Iterate from the last digit to the first
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits 
+            
+            digits[i] = 0
+            
+        return [1] + digits
